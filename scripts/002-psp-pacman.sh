4f9340bfeb5a054e17a42af0146a6562e6576411
example <179076459+StochasticEagle@users.noreply.github.com>
2026-08-30 17:17:10 -0600

    Removed Microsoft Windows build gating, issue fixed in psp-pacman.

--- scripts/002-psp-pacman.sh
+++ scripts/002-psp-pacman.sh
@@ -6,17 +6,11 @@ set -e
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 SOURCE="${ROOT}/components/psp-pacman"
 
-# TODO: Fix PACMAN compilation issues for Windows
-OSVER=$(uname)
-if [ "${OSVER:0:5}" == "MINGW" ]; then
-    exit 0
-fi
-
-if [ ! -f "${SOURCE}/pacman.sh" ]; then
+if [ ! -f "${SOURCE}/build-and-install.sh" ]; then
     echo "ERROR: psp-pacman submodule is not initialized."
     echo "Run: git submodule update --init --recursive --depth=1"
     exit 1
 fi
 
 cd "${SOURCE}"
-./pacman.sh
+./build-and-install.sh
