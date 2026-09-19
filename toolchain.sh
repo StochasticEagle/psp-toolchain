d16f645ff93c6ed00c0760019f7ade7e9d97a30a
Aaron <179076459+StochasticEagle@users.noreply.github.com>
2026-09-19 16:04:29 -0600

    Use permission-based install elevation

--- toolchain.sh
+++ toolchain.sh
@@ -3,7 +3,6 @@
 
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${ROOT}/install-permissions.sh"
-pspdev_require_unprivileged_build || exit 1
 
 ## Enter the psp-toolchain directory.
 cd "${ROOT}" || { echo "ERROR: Could not enter the psp-toolchain directory."; exit 1; }
